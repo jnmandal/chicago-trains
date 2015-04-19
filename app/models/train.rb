d01ad8beea2dde7519b35a1e1afe6a521c87bbd6
John N. Mandalakas <j.n.mandalakas@gmail.com>
2015-04-19 12:51:00 -0500

    add import class method to train model for reading from csv

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,3 +1,9 @@
 class Train < ActiveRecord::Base
   validates :run, uniqueness: true
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: %w[line route run operator]) do |row|
+      Train.create(row.to_hash)
+    end
+  end
 end
