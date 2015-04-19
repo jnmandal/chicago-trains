36d00703efbd73f98c7dff456ea44b36074e60c6
John N. Mandalakas <j.n.mandalakas@gmail.com>
2015-04-19 12:48:02 -0500

    add controller views and routes for trains

---
+++ app/controllers/trains_controller.rb
@@ -0,0 +1,64 @@
+class TrainsController < ApplicationController
+  before_action :set_train, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @trains = Train.all
+  end
+
+  def show
+  end
+
+  def new
+    @train = Train.new
+  end
+
+  def edit
+  end
+
+  def create
+    @train = Train.new(train_params)
+    respond_to do |format|
+      if @train.save
+        format.html { redirect_to @train, notice: 'Train was successfully created.' }
+        format.json { render :show, status: :created, location: @train }
+      else
+        format.html { render :new }
+        format.json { render json: @train.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @train.update(train_params)
+        format.html { redirect_to @train, notice: 'Train was successfully updated.' }
+        format.json { render :show, status: :ok, location: @train }
+      else
+        format.html { render :edit }
+        format.json { render json: @train.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @train.destroy
+    respond_to do |format|
+      format.html { redirect_to trains_url, notice: 'Train was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def import
+    Train.import(params[:file])
+    redirect_to root_url
+  end
+
+  private
+    def set_train
+      @train = Train.find(params[:id])
+    end
+
+    def train_params
+      params[:train]
+    end
+end
---
+++ app/views/trains/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @train, :id, :created_at, :updated_at
