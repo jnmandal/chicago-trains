2b87e0dbd1da767f6c60992cd9637d563e16abed
John N. Mandalakas <j.n.mandalakas@gmail.com>
2015-04-20 21:27:55 -0500

    improve import method to be CSV-header agnostic, add validation for stripping whitespace

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,15 +1,31 @@
 class Train < ActiveRecord::Base
+  before_validation :strip_whitespace
   validates :run, uniqueness: true
 
   def self.import(file)
-    CSV.foreach(file.path, headers: %w[line route run operator]) do |row|
-      Train.create(row.to_hash)
+    CSV.foreach(file.path, headers: true) do |row|
+      Train.create(
+        line:     row.field(0),
+        route:    row.field(1),
+        run:      row.field(2),
+        operator: row.field(3)
+      )
     end
   end
 
   def self.to_csv
     CSV.generate(headers: %w[TRAIN_LINE, ROUTE_NAME, RUN_NUMBER, OPERATOR_ID], write_headers: true) do |csv|
-      Train.all.each { |train| csv << [train.line, train.route, train.run, train.operator] }
+      self.all.each do |train|
+        csv << [train.line, train.route, train.run, train.operator]
+      end
     end
   end
+
+  private
+    def strip_whitespace
+      self.line     = self.line.strip     if self.line
+      self.route    = self.route.strip    if self.route
+      self.run      = self.run.strip      if self.run
+      self.operator = self.operator.strip if self.operator
+    end
 end
