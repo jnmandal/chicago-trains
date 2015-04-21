6e8c5c5053f029e715ead8fe11521fdd9852e205
John N. Mandalakas <j.n.mandalakas@gmail.com>
2015-04-21 17:09:56 -0500

    add additional tests for train flag

--- spec/models/trains_spec.rb
+++ spec/models/trains_spec.rb
@@ -24,6 +24,10 @@ describe Train do
     it "has an operator" do
       expect(train.operator).to be_a(String)
     end
+
+    it "has a flag" do
+      expect(train.flag).to be_a(FalseClass)
+    end
   end
 
   describe "validations" do
@@ -34,6 +38,53 @@ describe Train do
       expect(t2.save).to be(false)
     end
   end
+
+  describe "flags" do
+    it "doesn't flag a valid entry" do
+      expect(train.flag).to be(false)
+    end
+
+    it "flags invalid data in line" do
+      t = Train.create(
+        line:     "Netra",
+        route:    "Red Line",
+        run:      "E432",
+        operator: "LHill"
+      )
+      expect(t.flag).to be(true)
+    end
+
+    it "flags empty data in route" do
+      t = Train.create(
+        line:     "El",
+        route:     " ",
+        run:      "E432",
+        operator: "LHill"
+      )
+      expect(t.flag).to be(true)
+    end
+
+    it "flags empty data in run" do
+      t = Train.create(
+        line:     "El",
+        run:      " ",
+        route:    "Red Line",
+        operator: "LHill"
+      )
+      expect(t.flag).to be(true)
+    end
+
+    it "flags invalid data in run" do
+      t = Train.create(
+        line:     "El",
+        run:      "H22",
+        route:    "Red Line",
+        operator: "LHill"
+      )
+      expect(t.flag).to be(true)
+    end
+  end
+
 end
 
 
